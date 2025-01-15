885bf200a94aa46f8d4e6e8a5d43606272fd1909
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-15 08:43:13 +0100

    debut codage

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+// chargement de la page
+
+window.onload = function() {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.loader').style.transition='5s ease-out'
+  };
+ 
+ 
